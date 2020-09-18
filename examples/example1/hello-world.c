7f912e1f536c6ad29cfe0b3425b20264c6c9b67d
thorongil <officialantonio@outlook.it>
2020-09-18 21:20:36 +0200

    add files to dashboard edit the example

--- examples/example1/hello-world.c
+++ examples/example1/hello-world.c
@@ -1,9 +1,9 @@
-#include “contiki.h”
-#include “os/dev/button-hal.h”
+#include "contiki.h"
+#include "os/dev/button-hal.h"
 #include <stdio.h>
 
 /* Declare the process */ 
-PROCESS(hello_world_process, “Hello world”);
+PROCESS(hello_world_process, "Hello world");
 
 /* Make the process start when the module is loaded */ 
 AUTOSTART_PROCESSES(&hello_world_process);
@@ -20,19 +20,19 @@ PROCESS_THREAD(hello_world_process, ev, data)
     btn = button_hal_get_by_index(0);
 
     /* Setup a periodic timer that expires after 10 seconds. */
-    e_timer_set(&timer, CLOCK_SECOND * 15);
+    etimer_set(&timer, CLOCK_SECOND * 15);
     
     i = 0;
     while(1) {
         i++;
         /* Wait for the periodic timer to expire and then restart the timer. */
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired($timer));
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
         
         printf("Iteration Number: " + i);
         if(i == 10) {
             break;
         }
-        e_timer_reset(&timer);
+        etimer_reset(&timer);
     }
 
     PROCESS_END(); /* Must always come last */

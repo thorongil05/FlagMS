58779a27d70dfc84cf3b024d7223b581762d834c
thorongil <officialantonio@outlook.it>
2020-09-19 17:45:22 +0200

    add led color to led.c

--- examples/example-led/led.c
+++ examples/example-led/led.c
@@ -19,6 +19,18 @@ PROCESS_THREAD(led_project_process, ev, data)
         PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message);
         char* input = (char*)data;
         printf("received: %s\n", input);
+        if(strcmp(input, "green") == 0) {
+            leds_on(LEDS_NUM_TO_MASK(LEDS_GREEN))
+        }
+        if(strcmp(input, "green") == 0) {
+            leds_on(LEDS_NUM_TO_MASK(LEDS_RED))
+        }
+        if(strcmp(input, "green") == 0) {
+            leds_on(LEDS_NUM_TO_MASK(LEDS_YELLOW))
+        }
+        if(strcmp(input, "all") == 0) {
+            leds_on(LEDS_ALL)
+        }
     }
     
 

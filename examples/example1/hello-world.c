3dc0ce6c8401ed20eed7dff87084dfc409f35d16
thorongil <officialantonio@outlook.it>
2020-09-19 11:44:04 +0200

    fix bugs

--- examples/example1/hello-world.c
+++ examples/example1/hello-world.c
@@ -16,13 +16,12 @@ PROCESS_THREAD(hello_world_process, ev, data)
     static struct etimer timer;
     // button_hal_button_t *btn;
 
-    printf("Hello, world");
     // btn = button_hal_get_by_index(0);
 
-    /* Setup a periodic timer that expires after 10 seconds. */
+    /* Setup a periodic timer that expires after 15 seconds. */
     etimer_set(&timer, CLOCK_SECOND * 15);
-    
-    i = 0;
+    printf("Hello, world\n");
+    static int i = 0;
     while(1) {
         i++;
         /* Wait for the periodic timer to expire and then restart the timer. */

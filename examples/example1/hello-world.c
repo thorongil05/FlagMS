b8b8f294b6b30a31a3f218d7bf274137e50765a5
thorongil <officialantonio@outlook.it>
2020-09-19 12:06:53 +0200

    Add button to the example

--- examples/example1/hello-world.c
+++ examples/example1/hello-world.c
@@ -14,9 +14,9 @@ PROCESS_THREAD(hello_world_process, ev, data)
     PROCESS_BEGIN(); /* Must always come first */
 
     static struct etimer timer;
-    // button_hal_button_t *btn;
+    button_hal_button_t *btn;
 
-    // btn = button_hal_get_by_index(0);
+    btn = button_hal_get_by_index(0);
 
     /* Setup a periodic timer that expires after 10 seconds. */
     etimer_set(&timer, CLOCK_SECOND * 10);
@@ -28,11 +28,12 @@ PROCESS_THREAD(hello_world_process, ev, data)
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
         
         printf("Iteration Number: %d\n", i);
-        if(i == 10) {
+        if(ev == button_hal_press_event) {
+            printf("Stopped\n");
             break;
         }
         etimer_reset(&timer);
     }
-
+    printf("The end\n");
     PROCESS_END(); /* Must always come last */
 }

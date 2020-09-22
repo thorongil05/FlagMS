1a497c3148e0e131fc6534af76c67ca62950cde9
thorongil05 <officialantonio@outlook.it>
2020-09-22 10:07:04 -0400

    create californium project

---
+++ server/src/main/java/iot/unipi/it/App.java
@@ -0,0 +1,11 @@
+package iot.unipi.it;
+
+import org.eclipse.californium.core.CoapServer;
+
+public class App extends CoapServer {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+	
+}

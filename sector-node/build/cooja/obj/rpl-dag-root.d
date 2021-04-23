7e23ac7cfe0298402c8c30436004c01fb9aa405d
thorongil05 <a.acquavia@studenti.unipi.it>
2021-04-23 20:29:13 +0200

    start refactoring of the directory

---
+++ sector-node/build/cooja/obj/rpl-dag-root.d
@@ -0,0 +1,137 @@
+build/cooja/obj/rpl-dag-root.o: \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-dag-root.c \
+ ../../../contiki-ng/os/net/app-layer/coap/module-macros.h \
+ ../../../contiki-ng/os/contiki.h \
+ ../../../contiki-ng/arch/platform/cooja/./contiki-conf.h project-conf.h \
+ ../../../contiki-ng/os/contiki-default-conf.h \
+ ../../../contiki-ng/os/sys/process.h ../../../contiki-ng/os/sys/pt.h \
+ ../../../contiki-ng/os/sys/lc.h ../../../contiki-ng/os/sys/lc-switch.h \
+ ../../../contiki-ng/os/sys/cc.h ../../../contiki-ng/os/sys/cc-gcc.h \
+ ../../../contiki-ng/os/sys/autostart.h \
+ ../../../contiki-ng/os/sys/process.h ../../../contiki-ng/os/sys/timer.h \
+ ../../../contiki-ng/os/sys/clock.h ../../../contiki-ng/os/sys/ctimer.h \
+ ../../../contiki-ng/os/sys/etimer.h ../../../contiki-ng/os/sys/etimer.h \
+ ../../../contiki-ng/os/sys/rtimer.h \
+ ../../../contiki-ng/os/dev/watchdog.h \
+ ../../../contiki-ng/arch/platform/cooja/./rtimer-arch.h \
+ ../../../contiki-ng/arch/platform/cooja/./lib/simEnvChange.h \
+ ../../../contiki-ng/arch/platform/cooja/./sys/cooja_mt.h \
+ ../../../contiki-ng/arch/platform/cooja/./sys/cooja_mtarch.h \
+ ../../../contiki-ng/os/sys/pt.h ../../../contiki-ng/os/sys/clock.h \
+ ../../../contiki-ng/os/contiki-net.h ../../../contiki-ng/os/contiki.h \
+ ../../../contiki-ng/os/net/ipv6/tcpip.h \
+ ../../../contiki-ng/os/net/ipv6/uip.h \
+ ../../../contiki-ng/os/net/ipv6/uipopt.h \
+ ../../../contiki-ng/os/net/ipv6/uipbuf.h \
+ ../../../contiki-ng/os/net/linkaddr.h \
+ ../../../contiki-ng/os/net/ipv6/tcpip.h \
+ ../../../contiki-ng/os/net/ipv6/uip.h \
+ ../../../contiki-ng/os/net/ipv6/uiplib.h \
+ ../../../contiki-ng/os/net/ipv6/uip-udp-packet.h \
+ ../../../contiki-ng/os/net/ipv6/simple-udp.h \
+ ../../../contiki-ng/os/net/ipv6/uip-nameserver.h \
+ ../../../contiki-ng/os/net/routing/routing.h \
+ ../../../contiki-ng/os/net/ipv6/uip-ds6-nbr.h \
+ ../../../contiki-ng/os/net/ipv6/uip-nd6.h \
+ ../../../contiki-ng/os/sys/stimer.h \
+ ../../../contiki-ng/os/net/nbr-table.h \
+ ../../../contiki-ng/os/net/netstack.h \
+ ../../../contiki-ng/os/net/mac/mac.h ../../../contiki-ng/os/dev/radio.h \
+ ../../../contiki-ng/os/net/mac/framer/framer.h \
+ ../../../contiki-ng/os/net/ipv6/uip-packetqueue.h \
+ ../../../contiki-ng/os/sys/ctimer.h \
+ ../../../contiki-ng/os/net/ipv6/uip-ds6-route.h \
+ ../../../contiki-ng/os/lib/list.h \
+ ../../../contiki-ng/os/net/ipv6/uip-sr.h \
+ ../../../contiki-ng/os/net/ipv6/uip-icmp6.h \
+ ../../../contiki-ng/os/net/ipv6/uip-ds6.h \
+ ../../../contiki-ng/os/net/ipv6/psock.h \
+ ../../../contiki-ng/os/contiki-lib.h ../../../contiki-ng/os/lib/list.h \
+ ../../../contiki-ng/os/lib/memb.h ../../../contiki-ng/os/lib/random.h \
+ ../../../contiki-ng/os/net/ipv6/udp-socket.h \
+ ../../../contiki-ng/os/net/ipv6/tcp-socket.h \
+ ../../../contiki-ng/os/net/ipv6/uip.h \
+ ../../../contiki-ng/os/net/netstack.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-const.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-conf.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-types.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-icmp6.h \
+ ../../../contiki-ng/os/net/ipv6/uip.h \
+ ../../../contiki-ng/os/net/ipv6/uip-ds6.h \
+ ../../../contiki-ng/os/net/ipv6/uip-ds6-nbr.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-dag.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-dag-root.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-neighbor.h \
+ ../../../contiki-ng/os/net/ipv6/uip-ds6.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-ext-header.h \
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-timers.h \
+ ../../../contiki-ng/os/sys/log.h ../../../contiki-ng/os/sys/log-conf.h
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-dag-root.c :
+ ../../../contiki-ng/os/net/app-layer/coap/module-macros.h :
+ ../../../contiki-ng/os/contiki.h :
+ ../../../contiki-ng/arch/platform/cooja/./contiki-conf.h project-conf.h :
+ ../../../contiki-ng/os/contiki-default-conf.h :
+ ../../../contiki-ng/os/sys/process.h ../../../contiki-ng/os/sys/pt.h :
+ ../../../contiki-ng/os/sys/lc.h ../../../contiki-ng/os/sys/lc-switch.h :
+ ../../../contiki-ng/os/sys/cc.h ../../../contiki-ng/os/sys/cc-gcc.h :
+ ../../../contiki-ng/os/sys/autostart.h :
+ ../../../contiki-ng/os/sys/process.h ../../../contiki-ng/os/sys/timer.h :
+ ../../../contiki-ng/os/sys/clock.h ../../../contiki-ng/os/sys/ctimer.h :
+ ../../../contiki-ng/os/sys/etimer.h ../../../contiki-ng/os/sys/etimer.h :
+ ../../../contiki-ng/os/sys/rtimer.h :
+ ../../../contiki-ng/os/dev/watchdog.h :
+ ../../../contiki-ng/arch/platform/cooja/./rtimer-arch.h :
+ ../../../contiki-ng/arch/platform/cooja/./lib/simEnvChange.h :
+ ../../../contiki-ng/arch/platform/cooja/./sys/cooja_mt.h :
+ ../../../contiki-ng/arch/platform/cooja/./sys/cooja_mtarch.h :
+ ../../../contiki-ng/os/sys/pt.h ../../../contiki-ng/os/sys/clock.h :
+ ../../../contiki-ng/os/contiki-net.h ../../../contiki-ng/os/contiki.h :
+ ../../../contiki-ng/os/net/ipv6/tcpip.h :
+ ../../../contiki-ng/os/net/ipv6/uip.h :
+ ../../../contiki-ng/os/net/ipv6/uipopt.h :
+ ../../../contiki-ng/os/net/ipv6/uipbuf.h :
+ ../../../contiki-ng/os/net/linkaddr.h :
+ ../../../contiki-ng/os/net/ipv6/tcpip.h :
+ ../../../contiki-ng/os/net/ipv6/uip.h :
+ ../../../contiki-ng/os/net/ipv6/uiplib.h :
+ ../../../contiki-ng/os/net/ipv6/uip-udp-packet.h :
+ ../../../contiki-ng/os/net/ipv6/simple-udp.h :
+ ../../../contiki-ng/os/net/ipv6/uip-nameserver.h :
+ ../../../contiki-ng/os/net/routing/routing.h :
+ ../../../contiki-ng/os/net/ipv6/uip-ds6-nbr.h :
+ ../../../contiki-ng/os/net/ipv6/uip-nd6.h :
+ ../../../contiki-ng/os/sys/stimer.h :
+ ../../../contiki-ng/os/net/nbr-table.h :
+ ../../../contiki-ng/os/net/netstack.h :
+ ../../../contiki-ng/os/net/mac/mac.h ../../../contiki-ng/os/dev/radio.h :
+ ../../../contiki-ng/os/net/mac/framer/framer.h :
+ ../../../contiki-ng/os/net/ipv6/uip-packetqueue.h :
+ ../../../contiki-ng/os/sys/ctimer.h :
+ ../../../contiki-ng/os/net/ipv6/uip-ds6-route.h :
+ ../../../contiki-ng/os/lib/list.h :
+ ../../../contiki-ng/os/net/ipv6/uip-sr.h :
+ ../../../contiki-ng/os/net/ipv6/uip-icmp6.h :
+ ../../../contiki-ng/os/net/ipv6/uip-ds6.h :
+ ../../../contiki-ng/os/net/ipv6/psock.h :
+ ../../../contiki-ng/os/contiki-lib.h ../../../contiki-ng/os/lib/list.h :
+ ../../../contiki-ng/os/lib/memb.h ../../../contiki-ng/os/lib/random.h :
+ ../../../contiki-ng/os/net/ipv6/udp-socket.h :
+ ../../../contiki-ng/os/net/ipv6/tcp-socket.h :
+ ../../../contiki-ng/os/net/ipv6/uip.h :
+ ../../../contiki-ng/os/net/netstack.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-const.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-conf.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-types.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-icmp6.h :
+ ../../../contiki-ng/os/net/ipv6/uip.h :
+ ../../../contiki-ng/os/net/ipv6/uip-ds6.h :
+ ../../../contiki-ng/os/net/ipv6/uip-ds6-nbr.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-dag.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-dag-root.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-neighbor.h :
+ ../../../contiki-ng/os/net/ipv6/uip-ds6.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-ext-header.h :
+ ../../../contiki-ng/os/net/routing/rpl-lite/rpl-timers.h :
+ ../../../contiki-ng/os/sys/log.h ../../../contiki-ng/os/sys/log-conf.h :

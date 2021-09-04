ca56dc8a0fbd9ef36a46ff5361c7c16dbe99b52a
thorongil <officialantonio@outlook.it>
2021-09-04 17:49:20 +0200

    remove build folders

--- border-router/build/cooja/obj/nullradio.d
+++
@@ -1,8 +0,0 @@
-build/cooja/obj/nullradio.o: ../../../contiki-ng/os/dev/nullradio.c \
- ../../../contiki-ng/os/services/rpl-border-router/module-macros.h \
- ../../../contiki-ng/os/dev/nullradio.h \
- ../../../contiki-ng/os/dev/radio.h
-../../../contiki-ng/os/dev/nullradio.c :
- ../../../contiki-ng/os/services/rpl-border-router/module-macros.h :
- ../../../contiki-ng/os/dev/nullradio.h :
- ../../../contiki-ng/os/dev/radio.h :
--- tracklimit/build/cooja/obj/nullradio.d
+++
@@ -1,8 +0,0 @@
-build/cooja/obj/nullradio.o: ../../../contiki-ng/os/dev/nullradio.c \
- ../../../contiki-ng/os/net/app-layer/coap/module-macros.h \
- ../../../contiki-ng/os/dev/nullradio.h \
- ../../../contiki-ng/os/dev/radio.h
-../../../contiki-ng/os/dev/nullradio.c :
- ../../../contiki-ng/os/net/app-layer/coap/module-macros.h :
- ../../../contiki-ng/os/dev/nullradio.h :
- ../../../contiki-ng/os/dev/radio.h :

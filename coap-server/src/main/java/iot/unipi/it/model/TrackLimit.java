bff667330f0d9612e0442409aac1f42b45820849
Antonio Acquavia <39620897+thorongil05@users.noreply.github.com>
2021-09-16 21:14:54 +0200

    remove comment

--- coap-server/src/main/java/iot/unipi/it/model/TrackLimit.java
+++ coap-server/src/main/java/iot/unipi/it/model/TrackLimit.java
@@ -4,7 +4,6 @@ public class TrackLimit extends Resource {
 
 	public TrackLimit(String name, String path, String ip, boolean observable) {
 		super(name, path, ip, observable);
-		// TODO Auto-generated constructor stub
 	}
 
 
